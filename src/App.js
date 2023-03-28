9be7d76bd5394d7082d49409172c2b45a600d6a1
Jonathan Kim <vizard424@yahoo.com>
2023-03-28 17:50:42 -0600

    added footer info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components';
+import { Footer } from './containers';
 import { Homepage, Services, Pricing, Contact } from './pages';
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
           <Route path="/pricing" element={<Pricing />}/>
           <Route path="/contact" element={<Contact />}/>
         </Routes>
+        <div className='gradient__bg'>
+          <Footer />
+        </div>
       </div>
     </Router>
   );
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './footer.css';
+import { BsInstagram, BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <div className='wellness__footer' id='contact'>
-            Contact Me!
+        <div className='wellness__footer'>
+            <div className='wellness__footer-icons'>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
+                <a href="https://www.facebook.com/CircuitWorks" target="_blank" rel="noopener noreferrer"><BsFacebook /></a>
+            </div>
+            <div className='wellness__footer-copyright'>
+                <p>&#169; 2023 Circuit Works Wellness L.L.C. All rights reserved.</p>
+            </div>
         </div>
     )
 }
