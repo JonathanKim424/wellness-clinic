d16555ae87afa65f0c8c3ff19b93ffb61c2d0e68
Jonathan Kim <vizard424@yahoo.com>
2023-04-02 21:13:33 -0600

    created base files for services on homepage

---
+++ src/components/homeServices/HomeServices.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homeServices.css';
+
+const HomeServices = () => {
+    return (
+        <div className='wellness__homeServices'>
+            <p>This is a section!</p>
+        </div>
+    )
+}
+
+export default HomeServices
