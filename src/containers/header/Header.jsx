f50d263ea4747d24b4605b565ab7e9a163865ab5
Jonathan Kim <vizard424@yahoo.com>
2023-04-03 18:45:24 -0600

    added basic header with temp image

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './header.css';
+import image from '../../assets/header.jpg';
 
 const Header = () => {
     return (
         <div className='wellness__header'>
-            <div>
-                <p>Welcome to Circuit Works Wellness</p>
-                <p>We are offering vitamin injections, IV therapy, cold plunge and infrared therapy</p>
+            <div className='wellness__header-bg' style={{ backgroundImage: `url(${image})`}}>
+                <div className='wellness__header-text'>
+                    <p>Welcome to Circuit Works Wellness</p>
+                    <p>We are offering vitamin injections, IV therapy, cold plunge and infrared therapy</p>
+                </div>
             </div>
         </div>
     )
