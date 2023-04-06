16392a272d0bab6587e1736cf948b7b0ee9f4079
JonathanKim424 <vizard424@yahoo.com>
2023-04-06 15:09:10 -0600

    added content to contact page; add Google Map services

---
+++ src/components/gMap/GMap.jsx
@@ -0,0 +1,27 @@
+import { React, useMemo } from 'react';
+import './gMap.css';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+
+function GMap() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    });
+
+    if (!isLoaded) return <div>Loading...</div>;
+    return <Map />;
+}
+
+function Map() {
+    const center = useMemo(() => ({ lat: 34.01320173126053, lng:-118.48354153944086 }), []);
+    return (
+        <GoogleMap
+            zoom={14}
+            center={center}
+            mapContainerClassName="map-container"
+        >
+            <MarkerF position={center} />
+        </GoogleMap>
+    )
+}
+
+export default GMap
