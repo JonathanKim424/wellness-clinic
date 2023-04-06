60ad5db693c85d9d2c96ad0485fc28ff9055abda
JonathanKim424 <vizard424@yahoo.com>
2023-04-06 02:16:32 -0600

    added content to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './about.css';
+import profile from '../../assets/profile.jpg';
 
 function About() {
     return (
-        <div className='About'>
-            <div className='gradient__bg'>
-                <p>This it the About page!</p>
+        <div className='wellness__about'>
+            <div className='wellness__about-headshot'>
+                <img src={profile} alt="Profile" />
+            </div>
+            <div className='wellness__about-text'>
+                <p>
+                    Dr. Christine Kim completed her residency at
+                    Cedars-Sinai Medical Center and is board certified
+                    in Internal Medicine. She also has a certification
+                    in IV Nutrient Therapy. Having always been
+                    passionate about preventative care and wellness,
+                    Dr. Kim is trained in Integrative Medicine and has
+                    a strong interest in obesity medicine.
+                </p>
             </div>
         </div>
     )
