f17be0f9c3b94cc7a64a1cecc8bc51e15172673a
Jonathan Kim <vizard424@yahoo.com>
2023-06-19 17:24:57 -0600

    updated About and adjusted Contact page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './about.css';
-import profile from '../../assets/profile.jpg';
 
 function About() {
     return (
         <div className='wellness__about'>
-            <div className='wellness__about-headshot'>
-                <img src={profile} alt="Profile" />
-            </div>
             <div className='wellness__about-text'>
+                <h1>Our Mission</h1>
+                <p>
+                    At our health clinic, our mission is to empower our
+                    clients to achieve optimal health and wellness through
+                    a holistic approach to treatment. We specialize in
+                    providing vitamin injections, IV therapy, cold plunge,
+                    and infrared sauna therapy services, all of which are
+                    designed to promote detoxification, improve immune
+                    function, and enhance physical and mental performance.
+                    Our team of experienced professionals is committed to
+                    providing personalized care and expert guidance to
+                    help our clients feel their best and live their
+                    healthiest lives.
+                </p>
                 <h1>Dr. Christine Kim</h1>
                 <p>
                     Dr. Christine Kim completed her residency at
