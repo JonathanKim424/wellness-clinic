bfcd00da1fe9d75278ab3a46f4aca9f979e554d0
Jonathan Kim <vizard424@yahoo.com>
2023-07-22 12:52:29 -0600

    updated styling on service menu page

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './services.css';
+import { IvBlendCocktail } from '../../components';
 
 function Services() {
     return (
@@ -25,76 +26,46 @@ function Services() {
                             <h2>Vitamin Bar</h2>
                             <h3>Our Blends:</h3>
                             <div className='wellness__services-services-iv_body-content'>
-                                <div className='wellness__services-services-iv_body-content_header'>
-                                    <div className='wellness__services-services-iv_body-content_header-left'>
-                                        <h3>Athletic Support</h3>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header-right'>
-                                        <h3>$75</h3>
-                                    </div>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_body'>
-                                    <p>A blend of amino acids and nutrients to enhance muscle building, increase fat metabolism and boost athletic performance</p>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_header'>
-                                    <div className='wellness__services-services-iv_body-content_header-left'>
-                                        <h3>Recovery</h3>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header-right'>
-                                        <h3>$60</h3>
-                                    </div>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_body'>
-                                    <p>B vitamins and electrolytes to help you recover after that stellar workout</p>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_header'>
-                                    <div className='wellness__services-services-iv_body-content_header-left'>
-                                        <h3>Brain Sharpener</h3>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header-right'>
-                                        <h3>$115</h3>
-                                    </div>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_body'>
-                                    <p>Support your brain with NAD and B vitamins</p>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_header'>
-                                    <div className='wellness__services-services-iv_body-content_header-left'>
-                                        <h3>Chill Out</h3>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header-right'>
-                                        <h3>$70</h3>
-                                    </div>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_body'>
-                                    <p>Blend of amino acids and electrolytes to help relax your mind and body</p>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_header'>
-                                    <div className='wellness__services-services-iv_body-content_header-left'>
-                                        <h3>Skin Support</h3>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header-right'>
-                                        <h3>$60</h3>
-                                    </div>
-                                </div>
-                                <div className='wellness__services-services-iv_body-content_body'>
-                                    <p>Concoction of vitamins and minerals to support your hair, skin and nails</p>
-                                </div>
-                            </div>
-                            <div className='wellness__services-services-iv_body-content_footer'>
-                                <h3>Single Shots</h3>
-                                <div className='wellness__services-services-iv_body-content_footer-body'>
-                                    <div className='wellness__services-services-iv_body-content_footer-body_left'>
-                                        <p>Vitmain C</p>
-                                        <p>Vitamin B12</p>
-                                        <p>Glutathione</p>
-                                        <p>NAD</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_footer-body_right'>
-                                        <p>$35</p>
-                                        <p>$35</p>
-                                        <p>$60</p>
-                                        <p>$95</p>
+                                <IvBlendCocktail
+                                    name="Athletic Support"
+                                    description="A blend of amino acids and nutrients to enhance muscle building, increase fat metabolism and boost athletic performance"
+                                    price="$75"
+                                />
+                                <IvBlendCocktail
+                                    name="Recovery"
+                                    description="B vitamins and electrolytes to help you recover after that stellar workout"
+                                    price="$60"
+                                />
+                                <IvBlendCocktail
+                                    name="Brain Sharpener"
+                                    description="Support your brain with NAD and B vitamins"
+                                    price="$115"
+                                />
+                                <IvBlendCocktail
+                                    name="Chill Out"
+                                    description="Blend of amino acids and electrolytes to help relax your mind and body"
+                                    price="$70"
+                                />
+                                <IvBlendCocktail
+                                    name="Skin Support"
+                                    description="Concoction of vitamins and minerals to support your hair, skin and nails"
+                                    price="$60"
+                                />
+                                <div className='wellness__services-services-iv_body-content_footer'>
+                                    <h3>Single Shots</h3>
+                                    <div className='wellness__services-services-iv_body-content_footer-body'>
+                                        <div className='wellness__services-services-iv_body-content_footer-body_left'>
+                                            <p>Vitmain C</p>
+                                            <p>Vitamin B12</p>
+                                            <p>Glutathione</p>
+                                            <p>NAD</p>
+                                        </div>
+                                        <div className='wellness__services-services-iv_body-content_footer-body_right'>
+                                            <p>$35</p>
+                                            <p>$35</p>
+                                            <p>$60</p>
+                                            <p>$95</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -102,86 +73,50 @@ function Services() {
                                 <h2>IV Therapy</h2>
                                 <h3>Our Cocktails:</h3>
                                 <div className='wellness__services-services-iv_body-content'>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Myers Cocktail</h3>
+                                    <IvBlendCocktail
+                                            name="Myers Cocktail"
+                                            description="The classic cocktail with B vitamins, vitamin C and minerals for energy and well rounded support"
+                                            price="$199"
+                                    />
+                                    <IvBlendCocktail
+                                            name="Hangover Remedy"
+                                            description="Get perked back up after that crazy night"
+                                            price="$175"
+                                    />
+                                    <IvBlendCocktail
+                                            name="Athletic Recovery"
+                                            description="B vitamins, electrolytes and some extra hydration to help you recover after that stellar workout"
+                                            price="$125"
+                                    />
+                                    <IvBlendCocktail
+                                            name="Brain Sharpener"
+                                            description="Support your brain with NAD and B vitamins"
+                                            price="$225"
+                                    />
+                                    <IvBlendCocktail
+                                            name="Chill Out"
+                                            description="Relax your mind and your body with these amino acids and vitamins"
+                                            price="$175"
+                                    />
+                                    <IvBlendCocktail
+                                            name="Skin Support"
+                                            description="Concoction of vitamins and minerals to support your hair, skin and nails"
+                                            price="$175"
+                                    />
+                                    <div className='wellness__services-services-iv_body-content_footer'>
+                                        <h3>IV Boosters</h3>
+                                        <div className='wellness__services-services-iv_body-content_footer-body'>
+                                            <div className='wellness__services-services-iv_body-content_footer-body_left'>
+                                                <p>Vitmain C</p>
+                                                <p>Glutathione</p>
+                                                <p>NAD</p>
+                                            </div>
+                                            <div className='wellness__services-services-iv_body-content_footer-body_right'>
+                                                <p>$80</p>
+                                                <p>$175</p>
+                                                <p>$185</p>
+                                            </div>
                                         </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$199</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>The classic cocktail with B vitamins, vitamin C and minerals for energy and well rounded support</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Hangover Remedy</h3>
-                                        </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$175</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>Get perked back up after that crazy night</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Athletic Recovery</h3>
-                                        </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$125</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>B vitamins, electrolytes and some extra hydration to help you recover after that stellar workout</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Brain Sharpener</h3>
-                                        </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$225</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>Support your brain with NAD and B vitamins</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Chill Out</h3>
-                                        </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$175</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>Relax your mind and your body with these amino acids and vitamins</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_header'>
-                                        <div className='wellness__services-services-iv_body-content_header-left'>
-                                            <h3>Skin Support</h3>
-                                        </div>
-                                        <div className='wellness__services-services-iv_body-content_header-right'>
-                                            <h3>$175</h3>
-                                        </div>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_body'>
-                                        <p>Concoction of vitamins and minerals to support your hair, skin and nails</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='wellness__services-services-iv_body-content_footer'>
-                                <h3>IV Boosters</h3>
-                                <div className='wellness__services-services-iv_body-content_footer-body'>
-                                    <div className='wellness__services-services-iv_body-content_footer-body_left'>
-                                        <p>Vitmain C</p>
-                                        <p>Glutathione</p>
-                                        <p>NAD</p>
-                                    </div>
-                                    <div className='wellness__services-services-iv_body-content_footer-body_right'>
-                                        <p>$80</p>
-                                        <p>$175</p>
-                                        <p>$185</p>
                                     </div>
                                 </div>
                             </div>
